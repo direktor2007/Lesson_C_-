3d96172d43bbcbf9836b03a58e598888ec05c29b
direktor2007 <>
2023-02-06 11:04:57 +0700

    Examples003_Sum

---
+++ Exampeles/Examples003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// int numberA = 3;
+// int numberB = 5;
+// System.Console.WriteLine(numberA+numberB);
+
+int numberA = 31;
+int numberB = 5;
+int result = numberA+numberB;
+System.Console.WriteLine(result);
+
