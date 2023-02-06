3c72794b3b5100743a3f783d7b149ae10d155153
direktor2007 <>
2023-02-06 11:19:08 +0700

    random

--- Exampeles/Examples003_Sum/Program.cs
+++ Exampeles/Examples003_Sum/Program.cs
@@ -2,8 +2,14 @@
 // int numberB = 5;
 // System.Console.WriteLine(numberA+numberB);
 
-int numberA = 31;
-int numberB = 5;
+// int numberA = 31;
+// int numberB = 5;
+// int result = numberA+numberB;
+// System.Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10); // 1 2 3 ....9
+System.Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 1 2 3 ....9;
+System.Console.WriteLine(numberB);
 int result = numberA+numberB;
 System.Console.WriteLine(result);
-
